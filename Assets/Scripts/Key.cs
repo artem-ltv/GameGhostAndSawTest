ffe1adeda530a9ba9842abb4748fe057d486d2fd
artem-ltv <litwinenkoartem1@gmail.com>
2022-03-23 23:23:29 +0300

    S

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Key : MonoBehaviour
+{
+    [SerializeField] private Color _keyColor;
+
+    private SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.DOColor(_keyColor, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
