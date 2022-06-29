7bd6f15bcdf57076bd4390dc870cf7e24c9d38cb
artem-ltv <litwinenkoartem1@gmail.com>
2022-06-29 21:33:21 +0300

    Modification

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,12 +7,22 @@ public class Gun : MonoBehaviour
     [SerializeField] private Transform _pointSpawnBullet;
     [SerializeField] private float _offset;
     [SerializeField] private AudioClip _shotSound;
+    [SerializeField] private Player _player;
 
     private AudioSource _shootAudioSource;
+    private bool _isShooting;
 
-    private void Start() =>
+    private void Start()
+    {
         _shootAudioSource = GetComponent<AudioSource>();
-    
+        _isShooting = true;
+    }
+
+    private void OnEnable() =>
+        _player.PlayerDie += BlockShoot;
+
+    private void OnDisable() =>
+        _player.PlayerDie -= BlockShoot;
 
     private void Update()
     {
@@ -20,10 +30,14 @@ public class Gun : MonoBehaviour
         float rotateZ = Mathf.Atan2(diffrence.y, diffrence.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotateZ + _offset);
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(_bullet, _pointSpawnBullet.position, transform.rotation);
-            _shootAudioSource.PlayOneShot(_shotSound);
-        }
+        if (_isShooting)
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Instantiate(_bullet, _pointSpawnBullet.position, transform.rotation);
+                _shootAudioSource.PlayOneShot(_shotSound);
+            }     
     }
+
+    private void BlockShoot() =>
+        _isShooting = false;
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,38 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Player))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
 
+    private Player _player;
     private Vector3 _targetPosition;
+    private bool _isMovement;
+
+    private void Awake()
+    {
+        _player = GetComponent<Player>();
+        _isMovement = true;
+    }
+
+    private void OnEnable() =>
+        _player.PlayerDie += BlockMovement;
+
+    private void OnDisable() =>
+        _player.PlayerDie -= BlockMovement;
 
     private void Update()
     {
-        _targetPosition.x = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
-        _targetPosition.y = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+        if (_isMovement)
+        {
+            _targetPosition.x = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+            _targetPosition.y = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
 
-        transform.Translate(_targetPosition);
-        transform.rotation = Quaternion.identity;
+            transform.Translate(_targetPosition);
+            transform.rotation = Quaternion.identity;
+        }
     }
+
+    private void BlockMovement() =>
+        _isMovement = false;
 }
